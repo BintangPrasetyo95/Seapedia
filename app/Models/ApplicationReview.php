11dd6bd9a3802f87b0feae3f94222b008ed90bb8
bintangprasetyo95 <sutpidotdash@gmail.com>
2026-06-30 13:10:24 +0700

    fix: add missing user relationship to ApplicationReview model

--- app/Models/ApplicationReview.php
+++ app/Models/ApplicationReview.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ApplicationReview extends Model
 {
-    //
+    protected $fillable = ['user_id', 'rating', 'comment'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
